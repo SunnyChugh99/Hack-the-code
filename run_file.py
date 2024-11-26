9157782de75959d5efcb4029ed3ab1d11f78d7e3
amit.boke@fosfor.com <amit.boke@fosfor.com>
2024-11-26 08:39:25 +0000

    auto save by knights-watch

--- run_file.py
+++ run_file.py
@@ -3,11 +3,5 @@ import os
 # Print a separator for clarity.
 print('*----------------------------------*')
 
-# Print all environment variables.
-print(os.environ)
-
-# Print a separator for clarity.
-print('*----------------------------------*')
-
 # Access and print the 'HOME' environment variable.
 print(os.environ['HOME'])
