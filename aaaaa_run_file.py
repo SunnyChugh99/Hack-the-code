615c31e905b733f2d5a0ec6d5376f9fe9b6b04f8
amit.boke@fosfor.com <amit.boke@fosfor.com>
2024-11-28 11:42:34 +0000

    auto save by knights-watch

--- aaaaa_run_file.py
+++ aaaaa_run_file.py
@@ -1,22 +1,22 @@
-import os
+# import os
 
-# Print a separator for clarity.
-print('*----------------------------------*')
+# # Print a separator for clarity.
+# print('*----------------------------------*')
 
-# Access and print the 'HOME' environment variable.
-try:
-  print(os.environ['FDC_TEST'])
-except:
-  print("os.environ['FDC_TEST'] not availble")
+# # Access and print the 'HOME' environment variable.
+# try:
+#   print(os.environ['FDC_TEST'])
+# except:
+#   print("os.environ['FDC_TEST'] not availble")
 
-print('*RUN COMPLETE-----------------------*')
+# print('*RUN COMPLETE-----------------------*')
 
 
-import fosforml
-from fdc_connector_python_sdk.factory.connectorFactory import ConnectionFactory
-import sys
+# import fosforml
+# from fdc_connector_python_sdk.factory.connectorFactory import ConnectionFactory
+# import sys
 
-print(f'*Import working with - {sys.version}---------------*')
+# print(f'*Import working with - {sys.version}---------------*')
 
 
 
