2e5ab82c766940d8cd3d090b06de5756ce0fbfbc
amit.boke@fosfor.com <amit.boke@fosfor.com>
2024-12-02 09:34:42 +0000

    auto save by knights-watch

--- model_testing/sklearn_python.py
+++ model_testing/sklearn_python.py
@@ -40,3 +40,23 @@ register_model(
 )
 
 
+from sklearn.ensemble._forest import RandomForestClassifier
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+m_dataset = load_breast_cancer() 
+feature_names = m_dataset.feature_names
+u_features = [feature.replace(" ","_") for feature in feature_names]
+m_df = pd.DataFrame(m_dataset.data,columns=u_features)
+m_df["Target"] = m_dataset.target
+
+
+x_train,x_test,y_train,y_test = train_test_split(m_df.iloc[:,:-1],m_df['Target'])
+model = RandomForestClassifier()
+model.fit(x_train,y_train)
+
+y_pred = pd.DataFrame(model.predict(x_test),columns=["Predicted"])
+
+
+
