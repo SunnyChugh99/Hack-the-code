74c783b6bcb044c66647820385d92625ad55a12d
amit.boke@fosfor.com <amit.boke@fosfor.com>
2024-12-02 09:34:32 +0000

    auto save by knights-watch

--- model_testing/sklearn_python.py
+++ model_testing/sklearn_python.py
@@ -18,3 +18,25 @@ model = RandomForestClassifier()
 model.fit(x_train,y_train)
 
 y_pred = pd.DataFrame(model.predict(x_test),columns=["Predicted"])
+
+
+from fosforml import register_model
+register_model(
+  model_obj=model,
+  session=session,
+  x_train=x_train,
+  y_train=y_train,
+  x_test=x_test,
+  y_test=y_test,
+  y_pred=y_pred,
+    dataset_name = "iris",
+    dataset_source = "iris",
+  name="SklearnMulitClassModel",
+  description="This is a test sklearn model",
+  flavour="sklearn",
+  model_type="classification",
+  conda_dependencies=["scikit-learn==1.3.2"],
+    source="NOTEBOOK"
+)
+
+
