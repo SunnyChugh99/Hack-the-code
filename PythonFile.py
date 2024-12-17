cf9f84b608c1514c13dde5569e9781f3896a7cd9
saurabh.10686479@ltimindtree.com <saurabh.10686479@ltimindtree.com>
2024-12-17 08:08:33 +0000

    auto save by knights-watch

--- PythonFile.py
+++ PythonFile.py
@@ -14,16 +14,16 @@ class MyHandler(FileSystemEventHandler):
         mins_since_last_modified = (current_time - self.last_modified_time) / 60
         return mins_since_last_modified
 
-destination = '/notebooks/notebooks/'
+destination = '/path/to/your/directory'
 
+handler = MyHandler()  # Create the handler instance
 observer = Observer()
-observer.schedule(MyHandler(), destination, recursive=True)
+observer.schedule(handler, destination, recursive=True)  # Pass the handler instance to the observer
 observer.start()
 
 try:
     while True:
-        handler = observer.handlers[0]
-        mins_since_last_modified = handler.get_mins_since_last_modified()
+        mins_since_last_modified = handler.get_mins_since_last_modified()  # Use the handler instance to get the minutes since last modified
         print(f"Minutes since last modified: {mins_since_last_modified:.2f}")
         time.sleep(1)
 except KeyboardInterrupt:
