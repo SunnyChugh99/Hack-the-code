7d4b005621c155f67202ed4332452b728081815c
saurabh.10686479@ltimindtree.com <saurabh.10686479@ltimindtree.com>
2024-12-17 08:05:24 +0000

    auto save by knights-watch

--- PythonFile.py
+++ PythonFile.py
@@ -14,7 +14,7 @@ class MyHandler(FileSystemEventHandler):
         mins_since_last_modified = (current_time - self.last_modified_time) / 60
         return mins_since_last_modified
 
-destination = '/n'
+destination = '/notebooks/notebooks/'
 
 observer = Observer()
 observer.schedule(MyHandler(), destination, recursive=True)
