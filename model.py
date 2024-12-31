caef591cf7a2e43b8c2f4408029aad2649896fc8
saurabh.10686479@ltimindtree.com <saurabh.10686479@ltimindtree.com>
2024-12-31 05:51:46 +0000

    auto save by knights-watch

--- model.py
+++ model.py
@@ -1,15 +1,17 @@
-import timeclass 
-
-MyClass:
+import time
+ 
+class MyClass:
     def __init__(self):
         self.data = []
+ 
 def main():
     objects = []
     while True:
         obj = MyClass()
-        obj.data.append(list(range(1000000)))  # Append a large list        objects.append(obj)
+        obj.data.append(list(range(1000000)))  # Append a large list
+        objects.append(obj)
         print("aaaa")
         time.sleep(1)
-
+ 
 if __name__ == "__main__":
     main()
