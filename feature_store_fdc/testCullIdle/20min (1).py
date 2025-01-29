3170d2d4b690af31466c4c486de69c40c71542ab
saurabh.10686479@ltimindtree.com <saurabh.10686479@ltimindtree.com>
2025-01-29 14:51:38 +0000

    auto save by knights-watch

---
+++ feature_store_fdc/testCullIdle/20min (1).py
@@ -0,0 +1,11 @@
+import time
+print(" 10 sec Testing long job started ")
+
+time.sleep(10)
+#test234187889dsdsffdsd
+
+
+
+
+print(" 10 sec Testing long job ended ")
+print(" 10 sec Testing long job endeded 10 ")
